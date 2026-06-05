72930f0d369043ff8d177dfed46fc2779d09dfef
kayoung <pgy8404@naver.com>
2026-06-05 15:06:38 +0900

    fix: revert cors filter

--- src/main/java/cloudproject/com/global/config/SecurityConfig.java
+++ src/main/java/cloudproject/com/global/config/SecurityConfig.java
@@ -41,7 +41,6 @@ public class SecurityConfig {
                         .requestMatchers("/api/internal/**").permitAll()
                         .anyRequest().authenticated()
                 )
-                .addFilterBefore(new org.springframework.web.filter.CorsFilter(corsConfigurationSource()), UsernamePasswordAuthenticationFilter.class)
                 .addFilterBefore(internalTokenFilter, UsernamePasswordAuthenticationFilter.class)
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
